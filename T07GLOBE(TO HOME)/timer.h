def6bb6c16ee905f253286697b10efc19a179eb5
Frolov Konstantin (KF5) <frlovk12341@gmail.com>
2019-06-07 17:15:52 +0300

    globe for fun

---
+++ T07GLOBE(TO HOME)/timer.h
@@ -0,0 +1,8 @@
+LONG StartTime, OldTime, PauseTime, OldFPSTime, FrameCount;
+DOUBLE GLB_Time;
+DOUBLE GLB_DeltaTime;
+DOUBLE GLB_FPS;
+BOOL GLB_IsPause;
+
+VOID GLB_TimerInit( VOID );
+VOID GLB_TimerResponse( VOID );
