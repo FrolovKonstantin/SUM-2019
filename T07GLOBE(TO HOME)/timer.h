69e14baefaaf14f30d9d5a468273f8b0dc0e1c22
Frolov Konstantin (KF5) <frlovk12341@gmail.com>
2019-06-07 17:17:10 +0300

    Revert "globe for fun"
    This reverts commit def6bb6c16ee905f253286697b10efc19a179eb5.

--- T07GLOBE(TO HOME)/timer.h
+++
@@ -1,8 +0,0 @@
-LONG StartTime, OldTime, PauseTime, OldFPSTime, FrameCount;
-DOUBLE GLB_Time;
-DOUBLE GLB_DeltaTime;
-DOUBLE GLB_FPS;
-BOOL GLB_IsPause;
-
-VOID GLB_TimerInit( VOID );
-VOID GLB_TimerResponse( VOID );
